90c546c03d7736c35fbd44eaf198f358a8c6b2d6
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2025-11-25 14:44:13 +0100

    feat: added different bible version

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 # C-BIBLE
+
+A command Line interface Bible. 
+Defaults to KJV version.
+
+Project Stack
+------------------
+
+-   Python
+-   Click
+
+
+Installation
+------------
+
+1. Clone the repository: First, you will need to clone the Homemix repository to your local machine. This can be done using the following command in your terminal or command prompt:
+    ```bash
+    git clone https://github.com/vbello-tech/C-BIBLE.git
+    ```
+2. Create a virtual environment: It is recommended to work with a virtual environment to keep the dependencies for this project separate from other projects on your system. To create a virtual environment, run the following command:
+    ```bash
+    python -m venv venv
+    ```
+    Activate the virtual environment by running the following command:
+    ```bash
+    source venv/bin/activate (for Mac or Linux)
+    ```
+
+    ```bash
+    source venv\Scripts\activate (for Windows)
+    ```
+3. Install dependencies: Next, you will need to install the dependencies required for the project. These dependencies are listed in the requirements.txt file. To install them, run the following command:
+    ```bash
+    pip install -r requirements.txt
+    ```
+   
+Commands
+---------------
+
+1. Welcome message.
+    ```bash
+    python bible.py cbible
+    ```
+
+2. Request a bible passage.
+    ```bash
+    python bible.py request
+    ```
+   This prompts you to type in the Book, chapter and verse you want.
+
+3. Find a bible passage.
+    ```bash
+    python bible.py find job 3 16
+    ```
+   ```bash
+    python bible.py find job 3 16-20
+    ```
+   ```bash
+    python bible.py find job 3
+    ```
+   
+4. Find a bible passage for specific version.
+    ```bash
+    python bible.py find -v ESV job 3 16
+    ```
+   ```bash
+    python bible.py find -v ESV job 3 16-20
+    ```
+   ```bash
+    python bible.py find -v ESV job 3
+    ```
+
+Available Bible Versions
+---------------
+1. KJV 
+2. ESV
+3. ISV
+4. AKJV
+5. NIV
+6. NKJV
+7. NLT
+8. NRSV
+
+
+Todo
+------------
+- Word search.
+- Concordance.
+- Crossreference.
--- bible.py
+++ bible.py
@@ -4,7 +4,13 @@ import json
 
 from pathlib import Path
 
-bible_path = 'kjv.json'
+bible_path = 'bible-master/KJV_bible.json'
+
+
+def load_version(version):
+    version_bible_path = f'bible-master/{version}_bible.json'
+    with open(version_bible_path, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 
 def load_bible():
@@ -14,15 +20,15 @@ def load_bible():
 
 bible_reader = load_bible()
 
+rst = []
 
 @click.command()
 def cbible():
     click.echo("------------------------------------------------------------------------------------------------")
     name = input("\nWhat is your name?\n")
     click.echo(f"\nHello {name} \nWELCOME TO C-BIBLE\n"
-               f"\nYou can use the 'find' command to find a scripture following this format\n"
-               f"'python bible.py find job,3:16'\n"
-               f"'python bible.py find job,3:16-20'\n"
+               f"\nYou can use the 'read' command to find a scripture following this format\n"
+               f"'python bible.py read job 3 16'\n"
                f"\nOr the request command to input your bible request\n"
                f"'python bible.py request\n"
                f"\nThe Lord bless you as you go through His word.!!!\n")
@@ -36,92 +42,109 @@ def bible():
 
 @click.command()
 def request():
-    rst = []
-    m_rst = []
     bk = click.prompt("What book are you searching for?", type=str).strip().capitalize()
     chp = click.prompt("What chapter are you searching for?", type=str).strip()
     vs = click.prompt("What verse are you searching for?", type=str).strip()
 
-    if "-" in vs:
-        vs_l = vs.split('-')
-        vs_1st = vs_l[0]
-        vs_2nd = vs_l[1]
+    if vs:
+        if "-" in vs:
+            vs_l = vs.split('-')
+            vs_1st = vs_l[0]
+            vs_2nd = vs_l[1]
 
-        a = int(vs_1st)
-        b = int(vs_2nd) + 1
+            a = int(vs_1st)
+            b = int(vs_2nd) + 1
 
-        for n in range(a, b, +1):
-            for x in bible_reader:
-                if x["book"] == bk and x["chapter"] == chp and x["verse"] == str(n):
-                    m_rst.append(
-                        f"{x['book']} {x['chapter']}:{x['verse']}\n"
-                        f"  → {x['text']}"
-                    )
-        if not m_rst:
+            for n in range(a, b, +1):
+                for x in bible_reader:
+                    if x["book"] == bk and x["chapter"] == chp and x["verse"] == str(n):
+                        rst.append(
+                            f"{x['book']} {x['chapter']}:{x['verse']}\n"
+                            f"  → {x['text']}"
+                        )
+            if not rst:
                 click.secho("Verse not found. Check spelling and try again.", fg="red")
+            else:
+                click.secho("\nResult:\n", fg="green")
+                for r in rst:
+                    click.echo(r)
         else:
-            click.secho("\nResult:\n", fg="green")
-            for r in m_rst:
-                click.echo(r)
+            for x in bible_reader:
+                if x["book"] == bk and x["chapter"] == chp and x["verse"] == vs:
+                    rst.append(
+                        f"{x['book']} {x['chapter']}:{x['verse']}\n"
+                        f"→ {x['text']}\n"
+                    )
+            if not rst:
+                click.secho("Verse not found. Check spelling and try again.", fg="red")
+            else:
+                click.secho("\nResult:\n", fg="green")
+                click.echo(rst[0])
+
     else:
         for x in bible_reader:
-            if x["book"] == bk and x["chapter"] == chp and x["verse"] == vs:
+            if x["book"] == bk and x["chapter"] == chp:
                 rst.append(
                     f"{x['book']} {x['chapter']}:{x['verse']}\n"
                     f"→ {x['text']}\n"
                 )
-        if not rst:
-                click.secho("Verse not found. Check spelling and try again.", fg="red")
-        else:
-            click.secho("\nResult:\n", fg="green")
-            click.echo(rst[0])
+        click.secho("\nResult:\n", fg="green")
+        for text in rst:
+            click.echo(text)
 
 
 @click.command()
-@click.argument('text')
-def find(text):
-    rst = []
-    m_rst = []
+@click.option('-v', '--version', help="Find specific bible version")
+@click.argument('u_bk', )
+@click.argument('u_chp',)
+@click.argument('u_vs', required=False)
+def find(u_bk, u_chp, u_vs, version):
 
-    new_text = text.strip().split(',')
-    bk = new_text[0].strip().capitalize()
-    new_ch_text = new_text[1].strip().split(':')
-    chp = new_ch_text[0].strip()
-    vs = new_ch_text[1].strip()
+    version_reader = load_version(version) if version else bible_reader
 
-    if "-" in vs:
-        vs_l = vs.split('-')
-        vs_1st = vs_l[0]
-        vs_2nd = vs_l[1]
+    bk = u_bk.strip().capitalize()
+    chp = u_chp.strip()
+    vs = u_vs.strip() if u_vs else None
 
-        a = int(vs_1st)
-        b = int(vs_2nd) + 1
+    if vs:
+        if "-" in vs:
+            start, end = vs.split('-')
+            a = int(start)
+            b = int(end) +1
 
-        for n in range(a, b, +1):
-            for x in bible_reader:
-                if x["book"] == bk and x["chapter"] == chp and x["verse"] == str(n):
-                    m_rst.append(
-                        f"{x['book']} {x['chapter']}:{x['verse']}\n"
-                        f"  → {x['text']}"
-                    )
-        if not m_rst:
-                click.secho("Verse not found. Check spelling and try again.", fg="red")
-        else:
-            click.secho("\nResult:\n", fg="green")
-            for r in m_rst:
-                click.echo(r)
-    else:
-        for x in bible_reader:
-            if x["book"] == bk and x["chapter"] == chp and x["verse"] == vs:
+            for n in range(a, b, +1):
+                verse = str(n)
+                text = version_reader[bk][chp][verse]
                 rst.append(
-                    f"{x['book']} {x['chapter']}:{x['verse']}\n"
-                    f"→ {x['text']}\n"
+                    f"{bk} {chp}: {verse}\n"
+                    f"  → {text}\n"
                 )
-        if not rst:
+            if not rst:
                 click.secho("Verse not found. Check spelling and try again.", fg="red")
+            else:
+                click.secho("\nResult:\n", fg="green")
+                for r in rst:
+                    click.echo(r)
         else:
-            click.secho("\nResult:\n", fg="green")
-            click.echo(rst[0])
+            rst.append(
+                f"{bk} {chp}: {vs}\n"
+                f"  → {version_reader[bk][chp][vs]}"
+            )
+            if not rst:
+                click.secho("Verse not found. Check spelling and try again.", fg="red")
+            else:
+                click.secho("\nResult:\n", fg="green")
+                click.echo(rst[0])
+    else:
+        chapters = version_reader[bk][chp]
+        for vrs, txt in chapters.items():
+            rst.append(
+                f"{bk} {chp}: {vrs}\n"
+                f"  → {txt}\n"
+            )
+        click.secho("\nResult:\n", fg="green")
+        for text in rst:
+            click.echo(text)
 
 
 bible.add_command(cbible)
